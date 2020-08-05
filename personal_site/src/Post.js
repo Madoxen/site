1d13c03cfba534dd6a20fd5da22ad5294e045714
Madoxen <pchlad5@gmail.com>
2020-08-05 16:09:49 +0200

    Added HTML support in posts

--- personal_site/src/Post.js
+++ personal_site/src/Post.js
@@ -12,7 +12,7 @@ class Post extends React.Component {
       <div className="Post">
         <h1>{this.props.title}</h1>  {/* post title */}
         <p>{this.props.date}</p> {/*post date*/}
-        <p>{this.props.contents}</p>  {/* post content */}
+        <div dangerouslySetInnerHTML={{__html: this.props.contents}}></div>  {/* post content */}
       </div>
     );
   }
